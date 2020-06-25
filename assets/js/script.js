47bb2a446796a665ccb6595f92db407a2bf5365c
Kennadee <kennadee.ray@gmail.com>
2020-06-25 17:46:55 -0500

    styles added to start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
+var startButtonEl = document.getElementById("start-btn");
+var pageContentEl = document.querySelector("#page-content");
 var questionEl = document.getElementById("question-text");
+
 var aEl = document.getElementById("choice-one");
 var bEl = document.getElementById("choice-two");
 var cEl = document.getElementById("choice-three");
@@ -9,15 +12,22 @@ var question = [{q: "What does JSON stand for?",a: "Derulo???", b: "JavaScript O
                 {q: "Question 4!", a: "a", b: "b", c: "c", d: "d", correct: "c"},
                 {q: "Question 5!", a: "a", b: "b", c: "c", d: "d", correct: "d"}
 ];
-
+ var startQuiz = function(event) {
+     //upon click of sart button:
+     //timer starts counting down
+     //start screen is switched to no display
+     //question screen is displayed
+     //game listens for button clicks
+ }
 
 var questionLoop = function(answerChoice) {
-for (var i = 0; i <= (question.length - 1); i++) {
+for (var i = 1; i <= (question.length - 1); i++) {
     questionEl.textContent = question[i].q;
     aEl.textContent = question[i].a;
     bEl.textContent = question[i].b;
     cEl.textContent = question[i].c;
     dEl.textContent = question[i].d;
+
 
     
     if (answerChoice === question[i].correct) {
@@ -26,12 +36,32 @@ for (var i = 0; i <= (question.length - 1); i++) {
     else {
         console.log("Incorrect");
     }
-    
+
     
 }
 };
-aEl.addEventListener("click", questionLoop(aEl.textContent));
-bEl.addEventListener("click", questionLoop(bEl.textContent));
-cEl.addEventListener("click", questionLoop(cEl.textContent));
-dEl.addEventListener("click", questionLoop(dEl.textContent));
+var buttonHandler = function(event) {
+    var targetEl = event.target;
+
+    if (targetEl.matches("choice-one")) {
+      var answerChoice = document.getElementById("choice-one").textContent;
+      questionLoop(answerChoice);
+    }
+
+    else if (event.target.matches("choice-two")) {
+      var answerChoice = document.getElementById("choice-two").textContent;
+      questionLoop(answerChoice);
+    }
+    else if (event.target.matches("choice-three")) {
+        var answerChoice = document.getElementById("choice-three").textContent;
+        questionLoop(answerChoice);
+      }
+    else if(event.target.matches("choice-four")) {
+        var answerChoice = document.getElementById("choice-four").textContent;
+        questionLoop(answerChoice);
+    }
+};
+startButtonEl.addEventListener("click", startQuiz);
+
+
 
